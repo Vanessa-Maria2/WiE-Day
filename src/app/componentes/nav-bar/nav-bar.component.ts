e07816a463721fcb2b1c053b20d65a79fcf752ca
Vanessa-Maria2 <vanessa.silva5205@gmail.com>
2024-05-25 19:08:54 -0300

    feat: Responsive navigation bar implemented

--- src/app/componentes/nav-bar/nav-bar.component.ts
+++ src/app/componentes/nav-bar/nav-bar.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { Subscription } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive],
+  imports: [MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive, MatMenuModule, MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit, OnDestroy {
+  isHandset: boolean = false;
+  private breakpointSubscription: Subscription = Subscription.EMPTY;
 
+  @ViewChild(MatMenuTrigger) menuTrigger!: MatMenuTrigger;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointSubscription = this.breakpointObserver.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        this.isHandset = result.matches;
+        if (!this.isHandset && this.menuTrigger) {
+          this.menuTrigger.closeMenu();
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.breakpointSubscription.unsubscribe();
+  }
 }
