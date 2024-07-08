79eaa741a5451e96aba95b683032e5f49dba3727
= <vanessa.silva5205@gmail.com>
2024-07-07 23:16:20 -0300

    feat: added component to display partner content. #8

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { EventScheduleComponent } from './componentes/event-schedule/event-schedule.component';
 import { AboutComponent } from './componentes/about/about.component';
+import { AboutPartnerComponent } from './componentes/about-partner/about-partner.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'evento', component: AboutComponent},
     {path: 'programacao', component: EventScheduleComponent},
-    {path: 'empresas', component: HomeComponent},
+    {path: 'empresas', component: AboutPartnerComponent},
     {path: 'contato', component: HomeComponent},
     {path: 'inscricao', component: HomeComponent}
 ];
---
+++ src/app/componentes/about-partner/about-partner.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CardPartnerComponent } from '../card-partner/card-partner.component';
+import { Partner } from '../../interfaces/Partner';
+
+@Component({
+  selector: 'app-about-partner',
+  standalone: true,
+  imports: [CardPartnerComponent],
+  templateUrl: './about-partner.component.html',
+  styleUrl: './about-partner.component.css'
+})
+export class AboutPartnerComponent {
+  partners: Partner[] = [
+    { name: 'GPH', content: 'Grupo de extensão da UFRN dedicado a difundir o conhecimento científico e tecnológico. Oferecemos cursos tanto para o público interno quanto externo da UFRN, além de disponibilizar materiais de estudo no GitHub e Instagram. Também participamos e organizamos eventos focados em problemas relacionados à nossa área de pesquisa para aproximar a sociedade dos profissionais da área.' },
+    { name: 'MEG', content: 'O projeto MEG surgiu com o intuito de fazer um ambiente acadêmico e trabalhista na área das exatas mais agradável e convidativo para meninas e mulheres. O projeto atua com meninas de ensino médio, com oficinas de forma prática para desmitificar e descomplicar a matemática, ciências, computação e engenharias.' },
+    {
+      name: 'POTIGUAR ROCKET DESIGN', content: 'A Potiguar Rocket Design (PRD) é uma equipe voluntária de Tecnologias Aeroespaciais da UFRN, nascida em 2016, esse projeto de extensão tem 3 objetivos principais: ' +
+        '- Participar de competições na área aeroespacial, desenvolvendo foguetes e satélites;  🚀' +
+        '- Realizar oficinas, palestras e visitas para difusão do setor aeroespacial no RN; 🏫' +
+        '- Desenvolver produções científicas como materiais para eventos, revistas e congressos. 📝'
+    },
+    { name: 'POTYGUARA VERSE', content: 'O Potyguara Verse é uma startup que transforma a experiência de eventos online. Nascida durante a pandemia, nossa missão é democratizar o acesso à cultura global, apoiando artistas independentes e oferecendo uma plataforma inovadora. Mais do que um simples site de transmissão, proporcionamos uma experiência imersiva em realidade virtual (RV), onde artistas podem organizar eventos personalizados e monetizá-los de forma justa. Os usuários podem explorar ambientes interativos que simulam pontos turísticos, participar de atividades culturais, jogar minigames ou simplesmente assistir aos eventos de qualquer lugar, seja via óculos de RV ou em seus computadores. Combinamos realidades, integramos indústrias e utilizamos tecnologia de ponta para expandir a cultura pelo mundo.' },
+    {
+      name: 'GameLab', content: 'O Laboratório de Jogos Digitais e Educação (GameLab) destina-se à pesquisa e ao desenvolvimento de jogos digitais e tecnologias imersivas e interativas para a criação de soluções inovadoras, especialmente no setor educacional.\n' +
+        'O GameLab iniciou suas atividades em 2013, faz parte do Núcleo Integrador de Pesquisa e Inovação em Engenharia de Software(SETE) do Instituto Metrópole Digital(IMD), e é cadastrado no inventário de Laboratórios de Pesquisa da UFRN com o código LAB1747- 2017.\n' +
+        'Com uma abordagem interdisciplinar, as pesquisas desenvolvidas neste laboratório buscam aliar conhecimentos de Computação, Artes Visuais, Design, Música, Produção Audiovisual, Comunicação, Psicologia, entre outras áreas, buscando trazer inovações nas interfaces entre essas diferentes áreas de conhecimento a fim de formar profissionais qualificados para a área de jogos digitais.'
+    }
+  ];
+}
