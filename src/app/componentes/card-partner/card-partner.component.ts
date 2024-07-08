b45cfd921ead4ed7ac8898fb40d67cf7fb37777e
= <vanessa.silva5205@gmail.com>
2024-07-07 22:35:49 -0300

    feat: made adjustments to the partner card. #8

--- src/app/componentes/card-partner/card-partner.component.ts
+++ src/app/componentes/card-partner/card-partner.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { Partner } from '../../interfaces/Partner';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-partner',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './card-partner.component.html',
   styleUrl: './card-partner.component.css'
 })
 export class CardPartnerComponent {
-  @Input() name: string = '';
+  @Input() partners: Partner[] = [];
 }
