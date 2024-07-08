6667196d7900f694bca2ea8b09fdfc62feb72040
= <vanessa.silva5205@gmail.com>
2024-07-07 21:50:40 -0300

    feat: added event component. #7

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { EventScheduleComponent } from './componentes/event-schedule/event-schedule.component';
+import { AboutComponent } from './componentes/about/about.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: 'evento', component: HomeComponent},
+    {path: 'evento', component: AboutComponent},
     {path: 'programacao', component: EventScheduleComponent},
     {path: 'empresas', component: HomeComponent},
     {path: 'contato', component: HomeComponent},
